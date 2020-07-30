23c98c04cdd2349981ef8c9fe7c7754b31affeeb
Malvika Kumar <malvikakumar@Malvikas-MacBook-Pro.local>
2020-07-30 16:19:38 -0500

    Adding initial files

---
+++ Controllers/StatusController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TodoApi.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class StatusController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult Get()
+        {
+            return Content("Success");
+        }
+    }
+}
